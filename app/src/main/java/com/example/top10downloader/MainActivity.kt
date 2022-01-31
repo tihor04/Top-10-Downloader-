982cb95e99e6a6a38af5e7857c43ca8d396eb06c
Rohit singh <srrohitsingh1738@gmail.com>
2022-02-01 00:49:44 +0530

    version 1.1

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.top10downloader
 
-import android.content.ContentValues.TAG
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
+
 import java.net.URL
+
+class FeedEntry{
+    var name :String =""
+    var artist:String=""
+    var releaseDate:String=""
+    var summery :String=""
+    var imageurl:String=""
+
+    //we will use this toString function to see that if everything working fine in the logcat.
+
+    override fun toString(): String {
+        return """"
+            name= $name
+            artist= $artist
+            relaeasedate= $releaseDate
+            imageurl= $imageurl
+            """.trimIndent()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     private val TAG="mainActivity"
@@ -48,75 +61,16 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-            override fun onPostExecute(result: String?) {
+            override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG,"On post execute : parameter is $result")
+                val parseapplication=parseApplication()
+                parseapplication.parse(result)
             }
         }
 
 
         private fun downloadXML(urlPath:String?) :String{
-            val xmlReslut =StringBuilder()
-
-            try {
-                val url = URL(urlPath)
-                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                val response = connection.responseCode
-                Log.d(TAG, "downloadXML: the response code was: $response")
-//            val inputstream= connection.inputStream
-//            val inputStreamReader=InputStreamReader(inputstream)
-//            val reader=BufferedReader(inputStreamReader)
-
-
-//                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//
-//                val inputBuffer = CharArray(500)
-//                var charRead = 0
-//                while (charRead >= 0) {
-//                    charRead = reader.read(inputBuffer)
-//                    if (charRead > 0) {
-//                        xmlReslut.append(String(inputBuffer, 0, charRead))
-//
-//                    }
-//                }
-//                reader.close()
-
-                //better and simpler way to write the above code in kotlin
-
-                val stream=connection.inputStream
-                stream.buffered().reader().use{
-                    reader->xmlReslut.append(reader.readText())
-                }
-
-                Log.d(TAG, "Recived ${xmlReslut.length} bytes")
-                return xmlReslut.toString()
-            }catch(e:Exception)
-            {
-                val errormessage :String
-               errormessage= when(e){
-                    is MalformedURLException ->"downloadXML: Invalid url ${e.message}"
-                    is IOException -> "downloadXML:IO exception reading data ${e.message}"
-                    is SecurityException->"downloadXML: need permission ${e.message}"
-
-                   else -> "unknown error ${e.message}"
-               }
-            }
-
-//            }catch(e:MalformedURLException) {
-//                Log.e(TAG,"dowlnaldXML: malformed url : ${e.message}")
-//            }
-//            catch (e:IOException) {
-//                Log.e(TAG, "downloadXML:IO exception: ${e.message}")
-//            }
-//            catch(e:SecurityException){
-//                Log.e(TAG,"downloadXML: security exception : needs internet permision  ${e.message}")
-//            }
-//            catch(e:Exception) {
-//                Log.e(TAG,"unknown exception: ${e.message}")
-//            }
-
-
-            return ""  //if the program goes till here then we get an error
+          return URL(urlPath).readText()
         }
     }
 
---
+++ app/src/main/java/com/example/top10downloader/parseApplication.kt
@@ -0,0 +1,82 @@
+package com.example.top10downloader
+
+import android.service.autofill.TextValueSanitizer
+import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.lang.Exception
+
+class parseApplication {
+    private val TAG="pareseApplication"
+  val applications =ArrayList<FeedEntry>()
+
+
+    fun parse(XMLdata:String) :Boolean {
+        Log.d(TAG,"parse called for $XMLdata")
+        var status=true
+        var inEntry=false
+        var textValue=""
+
+        try{
+            val factory=XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware=true
+            val xpp=factory.newPullParser()
+            xpp.setInput(XMLdata.reader())
+            var eventType=xpp.eventType
+            var currentRecord=FeedEntry()
+
+            while(eventType!=XmlPullParser.END_DOCUMENT){
+                var tagName=xpp.name.toLowerCase() //TODO: we should use a safe call operator.
+                when(eventType) {
+
+                    XmlPullParser.START_TAG ->{
+                        Log.d(TAG, "Parse : end tag for $tagName")
+                    if (tagName == "entry") {
+                        inEntry = true
+                    }
+                }
+
+                    XmlPullParser.TEXT->textValue=xpp.text
+
+                    XmlPullParser.END_TAG->{
+                        Log.d(TAG,"parse:Ending tag for $tagName")
+                        if(inEntry){
+                            when(tagName){
+                                "entry"->{
+                                    applications.add(currentRecord)
+                                    inEntry=false
+                                    currentRecord=FeedEntry()//create a new obj
+
+                                }
+                                "name"->currentRecord.name=textValue
+                                "artist"->currentRecord.artist=textValue
+                                "releaseDate"->currentRecord.artist=textValue
+                                "summery"->currentRecord.artist=textValue
+                                "image"->currentRecord.imageurl=textValue
+
+                            }
+                        }
+                    }
+
+
+
+
+                }
+                eventType=xpp.next()
+            }
+            for(app in applications)
+            {
+                Log.d(TAG,"******************")
+                Log.d(TAG,app.toString())
+            }
+
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            status=false
+        }
+        return status
+
+    }
+
+}
